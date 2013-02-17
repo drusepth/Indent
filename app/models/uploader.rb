8e59fcb0bcc475b187fef1b8470005916c670941
Andrew Brown <ajbpn7@mst.edu>
2013-02-17 16:43:17 -0600

    move config file stuff

--- app/models/uploader.rb
+++ app/models/uploader.rb
@@ -1,18 +1,3 @@
-CarrierWave.configure do |config|
-
-  if Rails.env.production?
-    config.root = Rails.root.join('tmp')
-    config.cache_dir = 'carrierwave'
-
-    config.storage = :s3
-    config.s3_access_key_id = 'AKIAIWF4IBEMI6EQ3XOA'
-    config.s3_secret_access_key = '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'
-    config.s3_bucket = 'drusepth_Plan'
-  else
-    config.storage = :file
-  end
-end
-
 require 'carrierwave'
 #require 'rmagick'
 
