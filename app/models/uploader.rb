dd30bc3f0e9e3e76cefe442180f78f9b77450e82
Andrew Brown <ajbpn7@mst.edu>
2013-02-16 23:06:44 -0600

    add carrierwave config to uploader model

--- app/models/uploader.rb
+++ app/models/uploader.rb
@@ -1,3 +1,12 @@
+CarrierWave.configure do |config|
+  config.fog_credentials = {
+    :provider               => 'AWS',
+    :aws_access_key_id      => 'xxx',
+    :aws_secret_access_key  => 'yyy'
+  }
+  config.fog_directory  = 'zzz'
+end
+
 class Uploader
-  include Mongoid::Document
+  storage :fog
 end
