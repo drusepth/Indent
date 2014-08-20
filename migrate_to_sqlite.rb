c0d2be408e107fb060b52237bb0a90124022e7a1
Andrew Brown <drusepth@gmail.com>
2014-08-19 22:12:57 -0500

    Properly load and save all data

--- migrate_to_sqlite.rb
+++ migrate_to_sqlite.rb
@@ -17,23 +17,31 @@ def load_objects(klass, json_source_file)
     # Assign properties to instance
     json_object = JSON.parse line
     json_object.keys.each do |key|
+      key = (key == '_id' ? 'id' : key)
       memory_object[key] = json_object[key]
     end
 
     # Store object in memory to create links later
     objects << memory_object
-
-    #raise memory_object.inspect
   end
+
+  objects
 end
 
 def save_objects(id_translation_table, objects)
   objects.each do |object|
+    # Grab the new ID for association migrations in the next step
+    old_id = object.id
+
+    # Delete the non-sqlite keys before saving
+    object.id = nil
+
+    puts object.inspect
+
     # Save to DB
     object.save!
 
-    # Grab the new ID for association migrations in the next step
-    old_id = object._id
+    # Fetch the new ID and track old-->new
     id_translation_table[old_id] = object.reload.id
   end
 end
@@ -47,7 +55,7 @@ def update_links(id_translation_table, objects)
 end
 
 def path_for(klass)
-  "/home/ubuntu/dump/app10401744/#{klass}.json"
+  "/home/drusepth/dump/app10401744/#{klass}.json"
 end
 
 classes_to_migrate = %w(users characters equipment languages locations magics universes)
@@ -56,9 +64,14 @@ id_translation = {}
 
 db = {}
 classes_to_migrate.each do |klass|
+  puts "Loading #{klass} objects"
   db[klass] = load_objects(klass.singularize.titleize.constantize, path_for(klass))
 
+  puts "Saving #{klass} objects"
   save_objects(id_translation, db[klass])
-  update_links(id_translation, db[klass])
 end
 
+classes_to_migrate.each do |klass|
+  puts "Linking #{klass} objects"
+  update_links(id_translation, db[klass])
+end
