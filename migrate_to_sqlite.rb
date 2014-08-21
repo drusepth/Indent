c9ff8beb2c2a679ab1df50737b0d16b0c94749e1
Andrew Brown <drusepth@gmail.com>
2014-08-21 08:27:53 -0500

    Second shot at linking, almost there

--- migrate_to_sqlite.rb
+++ migrate_to_sqlite.rb
@@ -1,7 +1,8 @@
 require 'json'
 
-def load_objects(klass, json_source_file)
+def migrate_objects(klass, json_source_file, id_translation_table)
   objects = []
+  relink_queue = []
 
   File.open(json_source_file, "r").each_line do |line|
 
@@ -18,32 +19,27 @@ def load_objects(klass, json_source_file)
     json_object = JSON.parse line
     json_object.keys.each do |key|
       key = (key == '_id' ? 'id' : key)
+
+      # Track links that need updated to new IDs
+      if key.ends_with? '_id'
+        relink_queue << { object: memory_object, attribute: key, old_id: json_object['_id'] }
+        #puts "adding #{relink_queue.last.inspect}"
+      end
+
       memory_object[key] = json_object[key]
     end
 
     # Save object into DB
     memory_object.save!
+    memory_object.reload
 
-    puts "Translation: #{json_object['_id']} --> #{memory_object.id}"
+    id_translation_table[json_object['_id']] = memory_object.id
 
     # Store object in memory to create links later
     objects << memory_object
   end
 
-  objects
-end
-
-def update_links(id_translation_table, objects)
-  objects.each do |object|
-    object.attributes.each do |attr_name, attr_value|
-      if attr_name.ends_with? '_id'
-        puts "attr_name is #{attr_name}"
-        puts "value is #{attr_value}"
-        puts "Linking #{attr_name} to id #{id_translation_table[object.id]} for id #{object.id}"
-        object.send("#{attr_name}=" + id_translation_table[object.id].to_s)
-      end
-    end
-  end
+  relink_queue
 end
 
 def path_for(klass)
@@ -53,7 +49,22 @@ end
 classes_to_migrate = %w(users characters equipment languages locations magics universes)
 
 db = {}
+relink_queue = []
+id_translation_table = {}
 classes_to_migrate.each do |klass|
   puts "Loading #{klass} objects"
-  db[klass] = load_objects(klass.singularize.titleize.constantize, path_for(klass))
+  relink_events = migrate_objects(klass.singularize.titleize.constantize, path_for(klass), id_translation_table)
+  relink_queue << relink_events
+end
+
+# After saving everything, do another pass through the relink queue to update association IDs
+relink_queue.flatten.each do |relink_event|
+  object = relink_event[:object].reload
+  attribute = relink_event[:attribute]
+  old_id = relink_event[:old_id]
+  new_key = id_translation_table[old_id]
+
+  puts "Linking #{object.inspect} attribute #{attribute} from #{old_id} to #{new_key}"
+
+  object[attribute] = new_key
 end
